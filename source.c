c2342478bd231d29f45b62fc981c8c2433aa06ee
pappewaio <j.r.gadin@gmail.com>
2020-04-13 00:04:01 +0200

    minneshanteringsproblem finns. Förmodligen baserat på hur array är skapad

--- source.c
+++ source.c
@@ -5,28 +5,37 @@
 #include <unistd.h>  
 #include "Rmath.h"
 
-int operator_qnorm(char *buf) {
+//int operator_qnorm(char *buf) {
 //int operator_qnorm(char arrayvals[]) {
-//int operator_qnorm(char arrayvals[], int arraypositions[]) {
+int operator_qnorm(char *arrayvals, int arraypositions[]) {
+//  double prob = strtod(&arrayvals[arraypositions[1]], NULL);
+
 //  char *buf = arrayvals[arraypositions[0]]
-//  double prob = strtod(arrayvals[arraypositions[0]], NULL);
-    double prob = strtod(buf, NULL);
+//    double prob = strtod(buf, NULL);
 
-  if (errno != 0) {
-    perror("[ERROR] Failed to parse floating point number");
-    errno = 0;
+//  if (errno != 0) {
+//    perror("[ERROR] Failed to parse floating point number");
+//    errno = 0;
+//
+//    return 1;
+//  } else if (prob == 0.0 && strstr(buf, "0.0") == NULL) {
+//    fprintf(stderr, "[ERROR] Given line was not a floating point number: %s", buf);
+//
+//    return 1;
+//  }
 
-    return 1;
-  } else if (prob == 0.0 && strstr(buf, "0.0") == NULL) {
-    fprintf(stderr, "[ERROR] Given line was not a floating point number: %s", buf);
+  printf("%d\t", arraypositions[0]);
+  printf("%d\t", arraypositions[1]);
+  printf("%s\t", &arrayvals[0]);
+  printf("%s\t", &arrayvals[1]);
+  printf("%s\t", &arrayvals[2]);
+  printf("%s\t", &arrayvals[3]);
+  printf("%s\t", &arrayvals[4]);
+  printf("%s\t", &arrayvals[5]);
+  printf("%s\t", &arrayvals[6]);
+  printf("%s\n", &arrayvals[7]);
 
-    return 1;
-  }
-
-  //printf( "%s\t", arrayvals[*arraypositions[1]] );
-  //printf( "%d\t", arrayvals[1] );
-
-  printf("%lf\n", qnorm(prob, 0.0, 1.0, 1, 0));
+//  printf("%lf\n", qnorm(prob, 0.0, 1.0, 1, 0));
 
   return 0;
 }
@@ -58,7 +67,7 @@ int main(int argc, char *argv[]) {
   ssize_t bytes_read = 0;
 
   //int (*operator)(char*);
-  int (*operator)(char*);
+  int (*operator)(char*, int*);
 
   char operator_name[25] = "function_placeholder";
 
@@ -118,10 +127,9 @@ int main(int argc, char *argv[]) {
   if (argcol1 != 0) { argtot++;}
   if (argcol2 != 0) { argtot++;}
 
-  int* argcolvals[2] = {0};
-  if (argcol1 != 0) { argcolvals[0] = &argcol1;}
-  if (argcol2 != 0) { argcolvals[1] = &argcol2;}
-
+  int argcolvals[2] = {0};
+  if (argcol1 != 0) { argcolvals[0] = argcol1;}
+  if (argcol2 != 0) { argcolvals[1] = argcol2;}
 
 
   // return value if this function has no errors
@@ -154,7 +162,13 @@ int main(int argc, char *argv[]) {
     // be fine for this application as the pipeline can introduce NAs early in
     // the workflow
 
-    char *array[10];
+    // remove newline
+    int len;
+    len = strlen(buf);
+    if( buf[len-1] == '\n' )
+    buf[len-1] = 0;
+
+    char *array[10] = {NULL};
     int i=0;
     array[i] = strtok(buf,"\t");
     while(array[i]!=NULL) {
@@ -168,11 +182,15 @@ int main(int argc, char *argv[]) {
 
 
     //call operator function
-    //  printf( "%s\t", array[*argcolvals[1]] );
-    //  printf( "%d\t", *argcolvals[1] );
+      printf( "%d\t", argcolvals[0] );
+      printf( "%d\t", argcolvals[1] );
+      printf( "%s\t", array[0] );
+      printf( "%s\t", array[1] );
+      printf( "%s\t", array[2] );
+    //  printf( "%s\t", &array[argcolvals[0]] );
     //return_value = operator(array);
-    //return_value = operator(array, argcolvals);
-    return_value = operator(array[*argcolvals[0]]);
+    return_value = operator(*array, argcolvals);
+    //return_value = operator(array[argcolvals[0]]);
     //return_value = operator(buf);
 
     if (return_value != 0) {
