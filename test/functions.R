ae54d3e8c46aeb4a7f88ccd160ff12bf1593ae35
pappewaio <jesper.gadin@gmail.com>
2021-06-12 20:51:58 +0200

    quick variable fix in the R script. There was no bug right now, but it could have become one in the future

--- test/functions.R
+++ test/functions.R
@@ -8,7 +8,7 @@
 lin_zscore_from_pval_beta <- function(arr,inxs, mods){
   pval <- as.numeric(arr[inxs[1]])
   beta <- as.numeric(arr[inxs[3]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     pval = 10^-pval;
   }
   cat(sign(beta)*abs(qnorm(pval/2)))
@@ -18,7 +18,7 @@ lin_zscore_from_pval_beta_N <- function(arr,inxs, mods){
   pval <- as.numeric(arr[inxs[1]])
   beta <- as.numeric(arr[inxs[3]])
   N <- as.integer(arr[inxs[5]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     pval = 10^-pval;
   }
   cat(abs(qt(pval/2, N-2))*sign(beta))
@@ -43,7 +43,7 @@ lin_pval_from_zscore <- function(arr,inxs, mods){
 
 lin_pval_qnorm <- function(arr,inxs, mods){
   pval <- as.numeric(arr[inxs[1]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     pval = 10^-pval;
   }
   cat(qnorm(pval/2))
@@ -53,7 +53,7 @@ lin_beta_from_zscore_N_af <- function(arr,inxs, mods){
   zscore <- as.numeric(arr[inxs[6]])
   N <- as.integer(arr[inxs[5]])
   afreq <- as.numeric(arr[inxs[7]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     afreq = 1-afreq;
   }
   cat(zscore/sqrt(2*afreq*(1-afreq)*(N+zscore^2)))
@@ -75,7 +75,7 @@ lin_se_from_zscore_N_af <- function(arr,inxs, mods){
   zscore <- as.numeric(arr[inxs[6]])
   N <- as.integer(arr[inxs[5]])
   afreq <- as.numeric(arr[inxs[7]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     afreq = 1-afreq;
   }
   cat(1/sqrt(2*afreq*(1-afreq)*(N + zscore^2)))
@@ -85,7 +85,7 @@ lin_N_from_zscore_beta_af <- function(arr,inxs, mods){
   zscore <- as.numeric(arr[inxs[6]])
   beta <- as.numeric(arr[inxs[3]])
   afreq <- as.numeric(arr[inxs[7]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     afreq = 1-afreq;
   }
   cat(((zscore^2)/(2*afreq*(1-afreq)*beta^2))-zscore^2)
@@ -128,7 +128,7 @@ log_zscore_from_beta_se <- function(arr,inxs, mods){
 log_zscore_from_pval_oddsratio <- function(arr,inxs, mods){
   pval <- as.numeric(arr[inxs[1]])
   or <- as.numeric(arr[inxs[2]])
-  if (valmodifier[2] == 1) {
+  if (mods[2] == 1) {
     pval = 10^-pval;
   }
   cat(sign(or)*abs(qnorm(pval/2)))
